59ee48e5e411929763bc7fef76260da3f71e8ced
Ryan Halabi <ryanhalabi@gmail.com>
2020-01-19 18:04:08 -0700

    switch to c++/linux!

---
+++ build/bot/CMakeFiles/bot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../lib/libbot.dylib"
+  "../lib/libbot.pdb"
+  "CMakeFiles/bot.dir/Source/Dll.cpp.o"
+  "CMakeFiles/bot.dir/Source/ExampleAIModule.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
